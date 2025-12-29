85510448cf49576a45ed2a233f06ab9c5c3be512
soukaynaassila <assilasoukayna@gmail.com>
2025-12-29 09:52:19 +0000

    update chapitre 5

---
+++ Chapitre 5 – UPDATE, DELETE/realisation (1).sql
@@ -0,0 +1,18 @@
+/*UPDATE Syntax*/
+UPDATE table_name
+SET column1 = value1, column2 = value2, ...
+WHERE condition;
+
+/*The DELETE statement is used to delete existing records in a table.*/
+
+/*DELETE Syntax*/
+DELETE FROM table_name WHERE condition;
+
+/*To delete the table completely, use the DROP TABLE statement */
+/*Example
+Remove the Customers table:*/
+
+DROP TABLE Customers;
+
+
+
