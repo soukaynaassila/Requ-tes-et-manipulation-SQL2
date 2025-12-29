98a0a8f09d5d7b56a6e7db97b55d79726e89fed2
soukaynaassila <assilasoukayna@gmail.com>
2025-12-29 08:43:42 +0000

    update chapiter 4

---
+++ Chapitre 4 – INSERT INTO/realisation (1).sql
@@ -0,0 +1,8 @@
+/*If you are adding values for all the columns of the table,
+ you do not need to specify the column names in the SQL query. 
+ However, make sure the order of the values is in the same order as the columns in the table. Here,
+the INSERT INTO syntax would be as follows*/
+/*Example*/
+
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('Soukayna', 'Assilasoukayna', 'zyaten 21', 'Tanger', '4006', 'Morocco');
